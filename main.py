1978f75c1e25508f9a5606d57543799fb12778ee
Thainá Israel <91692393+DiegoCorredeira@users.noreply.github.com>
2023-02-18 14:06:17 -0300

    reestruturando projeto

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import PySimpleGUI as sg
 
-
-class Tela():
-    def __init__(self):
-        sg.theme('DarkAmber')
-        layout = [
-            [sg.Text(' Nome do filme', size=(15, 1)), sg.Input(key='Nome do Filme'), ],
-            [sg.Text('Nota para o filme', size=(15, 1)), sg.Input(key='Nota para o filme')],
-            [sg.Text('Data', size=(15, 1)), sg.InputText(key='Date'),
-             sg.CalendarButton("Calendário", close_when_date_chosen=True, target="Date", format='%d/%m/%Y',
-                               size=(10, 1))],
-
-            [sg.Submit('Enviar'), sg.Cancel('Sair')]
-
-        ]
-
-        janela = sg.Window("Dados do filme").layout(layout)
-
-        self.button, self.values = janela.Read()
-        if self.values in (sg.WIN_CLOSED, 'Cancel'):
-            exit()
-
-    def Iniciar(self):
-        print(self.values)
+theme = sg.theme('BlueMono')
 
 
-tela = Tela()
-tela.Iniciar()
+def primeiraTela():
+    flayout = [
+        [sg.Text('Bem vindo!')],
+        [sg.Text('Faça o cadastro dos filmes que já foram assistido.')],
+        [sg.Button('ENTRAR'), sg.Cancel('SAIR')]
+    ]
+
+    janela = sg.Window("Filmes", flayout, size=(500, 100), element_justification="center")
+
+    button, values = janela.Read()
+    if button == 'ENTRAR':
+        janela.close()
+    if button == 'SAIR':
+        exit()
+
+
+primeiraTela()
